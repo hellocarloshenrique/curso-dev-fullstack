adc778f13e4be5ae66964ef75428b29260fe48dd
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2022-04-25 21:27:47 -0300

    Operadores aritimeticos

---
+++ 9 - Javascript Basico/aula7/script.js
@@ -0,0 +1,50 @@
+/*
+
+adição: +
+subtração: -
+Multiplicação: *
+divisão: /
+incremento: ++
+decremento: --
+
+*/
+//NaN = Not a Number
+
+// adição
+let a = 10;
+let b = 10;
+let c = (a + b);
+console.log(c);
+
+
+// subtração
+let d = 10;
+let e = 101;
+let f = (d - e);
+console.log(f);
+
+// multiplicação
+
+let g = 10;
+let h = 101;
+let i = (g * h);
+console.log(i);
+
+// divisão
+
+let j = 10;
+let k = 101;
+let l = (j / k);
+console.log(l);
+
+// incremento
+
+let m = 10;
+m++
+console.log(m);
+
+// decremento
+
+let n = 10;
+n--
+console.log(n);
