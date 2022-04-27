7282c149687e617d63994e81f3d100d1d6bdb27f
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2022-04-27 20:51:47 -0300

    Condicionais

---
+++ 9 - Javascript Basico/aula9/script.js
@@ -0,0 +1,19 @@
+/*
+let idade = 15;
+
+if (idade > 18) {
+    document.write('Ok, você é maior de idade')
+} else if (idade === 15) {
+    document.write('tem 15')
+} else {
+    document.write('Você não pode.')
+}
+*/
+
+let nome = prompt('Digite seu nome')
+
+if (nome === 'Thiago') {
+    document.write(`Olá ${nome}, seja bem-vindo.`)
+} else {
+    document.write('Você não tem acesso de administrador.')
+}
