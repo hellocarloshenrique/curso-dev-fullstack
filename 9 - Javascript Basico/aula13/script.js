a6ef62f54dc74ab74d73060cdc1acc060e221241
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2022-07-29 07:08:57 -0300

    for of

---
+++ 9 - Javascript Basico/aula13/script.js
@@ -0,0 +1,17 @@
+let clientes = [
+    {
+        nome: "Carlos",
+        idade: 31,
+        Linguagem: ['javascript', 'kotlin', 'swift']
+    },
+    {
+        nome: "Elizabeth",
+        idade: 28,
+        Linguagem: ['do amor', 'raiva', 'hahahaha']
+    },
+    {
+        nome: "Elias",
+        idade: 31,
+        Linguagem: ['javascript', 'kotlin', 'swift']
+    },
+]
