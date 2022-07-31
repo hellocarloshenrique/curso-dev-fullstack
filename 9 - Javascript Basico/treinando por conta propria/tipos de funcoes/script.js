a9ad1e12374bb822d496a2ae937257cd5a670726
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2022-07-31 15:12:23 -0300

    Add files via upload

---
+++ 9 - Javascript Basico/treinando por conta propria/tipos de funcoes/script.js
@@ -0,0 +1,19 @@
+//essa função recebe um valor e não retorna nada
+function dizerNome() {
+    console.log('Carlos Henrique');
+}
+
+dizerNome();
+
+// -----------------------------------------------
+
+//faz um calculo e devolve algo
+function multiplicarPorDois(valor) {
+    return valor * 2;
+}
+
+/* console.log(multiplicarPorDois(570)) */
+
+let resultado = multiplicarPorDois(570);
+
+console.log(resultado);
