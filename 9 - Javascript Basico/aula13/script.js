e6c9150ae195bc07dbdc16c6cb69e4356bbf5b19
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2022-07-31 12:39:48 -0300

    usando for of

--- 9 - Javascript Basico/aula13/script.js
+++ 9 - Javascript Basico/aula13/script.js
@@ -2,16 +2,44 @@ let clientes = [
     {
         nome: "Carlos",
         idade: 31,
-        Linguagem: ['javascript', 'kotlin', 'swift']
+        linguagem: ['javascript', 'kotlin', 'swift']
     },
     {
         nome: "Elizabeth",
         idade: 28,
-        Linguagem: ['do amor', 'raiva', 'hahahaha']
+        linguagem: ['do amor', 'raiva', 'hahahaha']
     },
     {
         nome: "Elias",
         idade: 31,
-        Linguagem: ['javascript', 'kotlin', 'swift']
-    },
+        linguagem: ['javascript', 'kotlin', 'swift']
+    }
 ]
+
+/* for (let contador = 0; contador < clientes.length; contador++){
+    console.log(clientes[contador].idade)
+} */
+
+let htmlClientes = ''
+
+for (let cliente of clientes){
+
+    let listaLinguagens = ''
+
+    for (let linguagem of cliente.linguagem) {
+        listaLinguagens += `<li>${linguagem} </li>`
+    }
+
+    htmlClientes += `
+        <li>
+            <b>Nome:</b> ${cliente.nome} <br>
+            <b>Idade:</b> ${cliente.idade}<br>
+            <b>Linguagem:</b><br>
+            <ul>
+                ${listaLinguagens}
+            </ul>
+        </li>
+    `
+}
+
+document.querySelector('#listaClientes').innerHTML = htmlClientes;
