0d5878ab1866123639218f1e98939d7fc6f30772
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2022-08-03 07:32:21 -0300

    conhecendo tolowercase

---
+++ 9 - Javascript Basico/exercicio 02/script.js
@@ -0,0 +1,22 @@
+
+let nome = prompt("Olá! Digite seu nome: ") 
+let idade = prompt("Qual a sua idade?")
+
+
+/* if(idade < 19) {
+    document.write(`Olá, ${nome}! Você é menor de idade.`)
+} else if {
+    document.write(`Olá, ${nome}! Você é maior de idade.`)
+}
+} */
+
+
+if(nome.toLowerCase() == 'neo' && idade < 18) {
+    document.write(`Olá, ${nome}! Você é menor de idade. Você é personagem do filme Matrix!`)
+} else if(nome.toLowerCase() == 'neo' && idade >= 18) {
+    document.write(`Olá, ${nome}! Você é maior de idade. Você é personagem do filme Matrix!`)
+}else if(idade < 19) {
+    document.write(`Olá, ${nome}! Você é menor de idade.`)
+} else{
+    document.write(`Olá, ${nome}! Você é maior de idade.`)
+}
