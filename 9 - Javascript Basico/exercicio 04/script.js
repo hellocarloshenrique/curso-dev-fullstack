bb476dd2b1f2d38fc93b75d7ce01519dd64afe44
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2022-08-09 22:07:48 -0300

    Add files via upload

---
+++ 9 - Javascript Basico/exercicio 04/script.js
@@ -0,0 +1,27 @@
+
+function entrada() {
+    let nome = prompt("Olá! Digite seu nome: ") 
+    let idade = prompt("Qual a sua idade?")
+        
+
+    if (nome === '' || idade === '') {
+        alert("Preencha os campos corretamente!")
+        return entrada
+    }else{
+        
+        if(nome.toLowerCase() == 'neo' && idade < 18) {
+            document.write(`Olá, ${nome}! Você é menor de idade. Você é personagem do filme Matrix!`)
+        } else if(nome.toLowerCase() == 'neo' && idade >= 18) {
+            document.write(`Olá, ${nome}! Você é maior de idade. Você é personagem do filme Matrix!`)
+        }else if(idade < 19) {
+            document.write(`Olá, ${nome}! Você é menor de idade.`)
+        } else{
+            document.write(`Olá, ${nome}! Você é maior de idade.`)
+        }
+    } 
+}
+
+
+//chamei a função entrada dentro do index.html com "<button onclick="entrada()">Entrar</button>"
+
+    
