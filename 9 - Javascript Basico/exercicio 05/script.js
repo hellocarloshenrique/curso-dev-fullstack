592e0b35cd427e39234b6c2073d1ab41170986a6
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2022-08-23 06:04:31 -0300

    criando uma tabuada usando if

---
+++ 9 - Javascript Basico/exercicio 05/script.js
@@ -0,0 +1,11 @@
+
+let qualNumero = prompt("Qual tabuada você gostaria de saber?")
+
+
+for(let tabuada = 1; tabuada <=10; tabuada++) {
+
+    resultado = qualNumero * tabuada
+    document.write(`${qualNumero} x ${tabuada} = ${resultado} <br>`)
+    
+}
+
