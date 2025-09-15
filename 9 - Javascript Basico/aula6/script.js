a63854a64e59dcee5bc983b78c2ebe92da60dfb7
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2025-09-15 18:34:03 -0300

    Add files via upload

---
+++ 9 - Javascript Basico/aula6/script.js
@@ -0,0 +1,38 @@
+// var, let e const
+
+ //string
+var nome = 'Carlos'
+
+ //number
+var idade = 36
+
+//boolean
+let lampadaAcesa = true
+
+//array
+let habilidades = ['javascript', 'php', 'python']
+
+// objeto
+let pessoa = {
+     nome: 'Carlos',
+     sobrenome: 'Damasceno',
+     idade: 33,
+     habilidades: ['C++', 'C#', 'Python']
+}
+
+
+console.log(nome);
+console.log(idade);
+console.log(lampadaAcesa);
+
+console.log(`${nome} ${idade}`);
+
+
+console.log(habilidades)
+console.log(habilidades.length)
+console.log(habilidades[0])
+console.log(pessoa.habilidades[2]);
+
+
+
+
