68c6da9c711eac95a70a462cc767dc52b6dc4c78
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2025-09-15 22:39:09 -0300

    Add files via upload

---
+++ 9 - Javascript Basico/aula8_operadores_comparativos/script.js
@@ -0,0 +1,26 @@
+/*
+ == igualdade de valores
+
+ === igualdade valores e tipos
+
+ != diferenca de valores
+
+ !== diferenca de valores e tipo
+
+ > maior que
+
+ < menor que
+*/
+
+let a = 10
+let b = 10
+let c = '10'
+console.log(a == b );
+console.log(a === c); //valor igual ,mas tipos diferentes
+console.log(a == c); //valor igual , nao considera o tipo
+console.log(a != c); // diferenca de valores apenas
+console.log(a !== c); // verifica se ha diferenca entre valor e tipos
+
+
+
+
