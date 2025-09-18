c5108b661bed91912c15215dd6d1d7fe0b7beefe
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2025-09-18 06:57:45 -0300

    Add files via upload

---
+++ 9 - Javascript Basico/aula14_while/script.js
@@ -0,0 +1,9 @@
+
+let contador = 0
+
+while(contador < 10) {
+     console.log('Hello world');
+     document.writeln('Hello world <br>');
+     
+     contador++
+}
---
+++ 9 - Javascript Basico/aula15_functions/script.js
@@ -0,0 +1,8 @@
+
+
+function escreverNoConsole(mensagem, nome) {
+     console.log(`Ola ${nome}, ${mensagem}`);
+     
+}
+
+console.log('Seja muito bem-vindo(a)', 'Carlinhos');
