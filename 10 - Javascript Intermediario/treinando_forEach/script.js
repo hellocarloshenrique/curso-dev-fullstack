315b5fc88a08d325273c0e3f1ac91fce3f616d55
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2025-09-23 19:07:08 -0300

    Add files via upload

---
+++ 10 - Javascript Intermediario/treinando_forEach/script.js
@@ -0,0 +1,28 @@
+const amigos = ['Elias', 'Carlos', 'Samuel']
+
+amigos.forEach(function(amigo) {
+    console.log(`Ola, ${amigo}`);
+    
+})
+
+
+const amigas = ['Beth', 'Geska', 'Illany']
+
+amigas.forEach(amiga => {
+    console.log(`Ola, ${amiga}`);
+    
+})
+
+
+const colegas = ['Diesom', 'Karol', 'Sthezinha']
+
+colegas.forEach(function(colega) {
+    console.log(colega);
+    
+})
+
+const nike = ['Dunk', 'Air Force 1', 'Air Jordan']
+
+nike.forEach(meuNike => {
+    console.log(meuNike);
+})
