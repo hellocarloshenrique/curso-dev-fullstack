61c0bca3c3c4393640cbdd20594a4b8d227b7b8e
Carlos R. Damasceno <carlos.damasceno182@gmail.com>
2025-10-16 06:31:50 -0300

    Filter e IF ternario

---
+++ 12 -  javascript avancado/aula5_filter/script.js
@@ -0,0 +1,38 @@
+const clientes = [
+    {name: 'Carlos', lastname: 'Damasceno', age: 14},
+    {name: 'Elizabeth', lastname: 'Damasceno', age: 18},
+    {name: 'Elias', lastname: 'Ferreira', age: 20},
+]
+
+//filtgrando clientes com idade igual/acima de 18
+const clientesMaiorIdade = clientes.filter(cliente => {
+    let retorno = false
+    if (cliente.age >= 18) {
+        return true  
+    }
+
+    return retorno
+})
+
+console.log(clientesMaiorIdade);
+
+
+//filtrando clientes com o sobrenome Damasceno
+const clientesDamasceno = clientes.filter(cliente => {
+    let retorno = false
+    if (cliente.lastname === 'Damasceno') {
+       return true 
+    }
+
+    return retorno
+})
+
+console.log(clientesDamasceno)
+
+//IF Ternario (IF de uma linha so)
+
+const clientesCarlos = clientes.filter(cliente => {
+    return cliente.name === 'Carlos' ? true : false 
+})
+
+console.log(clientesCarlos);
