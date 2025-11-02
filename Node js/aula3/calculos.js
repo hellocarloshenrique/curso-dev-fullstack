66c97c60883e2d6ab2eb5f3f7d395f29b51637fc
Carlos Damasceno <carlos.damasceno182@gmail.com>
2025-11-02 19:52:55 -0300

    Node  modulos

---
+++ Node js/aula3/calculos.js
@@ -0,0 +1,15 @@
+
+// funcao de adicao
+function soma(n1, n2) {
+    return  n1 + n2
+}
+
+//  Funcao de multiplicacao
+function multiplicacao(n1, n2) {
+    return  n1 * n2
+}
+
+module.exports = {
+    soma,
+    multiplicacao
+}
---
+++ Node js/aula3/index.js
@@ -0,0 +1,10 @@
+const calculos = require('./calculos.js')
+
+
+
+const result1 =  calculos.soma(10, 20)
+console.log(result1);
+
+const  result2 = calculos.multiplicacao(20, 20)
+console.log(result2);
+
