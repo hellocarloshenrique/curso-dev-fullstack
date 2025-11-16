8f7ab69a562e4bb7fbb7f67da4c4bca9ce06df10
Carlos Damasceno <carlos.damasceno182@gmail.com>
2025-11-16 15:10:27 -0300

    mais ex. desestruturacao

---
+++ Treinando javascript/treinando_desestruturacao/desestruturacao_objetos2.js
@@ -0,0 +1,10 @@
+const contatoTripulante = {
+    nome: 'Link',
+    indicativo: '01',
+    nave: 'nabucodonossor',
+    telefone: '555-LINK-32'
+}
+
+const { nome, telefone } = contatoTripulante
+
+console.log(`Contato: ${nome} - ${telefone}`);
